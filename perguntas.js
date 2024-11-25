b113c548f90031acc66bd84ced6149592b1cf576
mariamarcela01 <fariamariamarcela01@gmail.com>
2024-11-25 09:49:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'O que é um  Pulsar?',
+    'Pulsar é uma estrela de nêutrons que, em virtude de seu intenso campo magnético (da ordem de 108 T), transforma a energia rotacional em energia eletromagnética.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual a massa de Sagittarius A?',
+    'A pesquisa, publicada recentemente na revista Astronomy & Astrophysics, foi realizada pelo Instituto Max Planck de Física Extraterrestre (MPE) e determinou com precisão que o Sagittarius A* possui 4297 milhões de massas solares e um raio menor que a órbita de Vênus em torno do Sol.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quanto é a gravidade da lua?',
+    '1,62 m/s², Na Terra, a aceleração da gravidade é de 9,8 m/s2. Já na Lua, é de 1,6 m/s2 – cerca de um sexto da aceleração gravitacional do nosso planeta.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual a importância do campo magnético da Terra?',
+    'O campo magnético da Terra é de suma importância para a manutenção da vida e da comunição terrestres, já que ele consegue evitar a entrada das partículas solares, as quais, caso não fossem desviadas por ele, danificariam a camada de ozônio, desprotegendo a Terra da radiação ultravioleta e dificultando a comunicação'
+)
